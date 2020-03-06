143f7ea750a6cfccf254b1d2fdc440a08df38ece
Tarique Memon <tmemon@chemcentre.wa.gov.au>
2020-03-06 11:00:54 +0800

    first commit

---
+++ SignaShell/App.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using ZetaIpc.Runtime.Server;
+
+namespace SignaShell
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var s = new IpcServer();
+            s.Start(12345); 
+
+            MainWindow mainWindow = new MainWindow();            
+            mainWindow.Show();
+
+            s.ReceivedRequest += (initiator, args) =>
+            {                  
+                Dispatcher.Invoke(new Action(() => 
+                {
+                    var windowExists = Application.Current.Windows.OfType<Window>().Any(w => w.GetType().FullName == args.Request);
+
+                    if(!windowExists)
+                    {
+                        var window = (Window)Activator.CreateInstance(Type.GetType(args.Request));
+                        window.Show();
+                    }
+
+                    var windows = Application.Current.Windows.OfType<Window>();                    
+                    foreach(var window in windows)
+                    {
+                        if(window.GetType().FullName != args.Request)
+                        {
+                            window.Close();
+                        }
+                    }
+                }));
+            };
+        }
+
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            e.Handled = true;
+        }
+    }
+}
---
+++ SignaShell/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace SignaShell
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        string _message;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        public string Arguments
+        {
+            get
+            {
+                return _message;
+            }
+            set
+            {
+                _message = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
